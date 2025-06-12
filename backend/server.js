e9cac361983725aed62180bcab4e62031b827aa5
Anuj <bpro2855203@gmail.com>
2025-06-12 15:12:11 +0530

    Updated mongo connection

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 // ✅ Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB Connection Error:', err));
 
